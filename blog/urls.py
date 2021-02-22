93aa6ce6674a1c2a9f684678acac3b66a2344a48
Adrian <weber.adrian27@gmail.com>
2021-02-22 13:31:22 +0100

    Modified templates to display posts from database.

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,6 @@
 from django.urls import path
 from . import views
 
-<<<<<<< HEAD
 urlpatterns = [
     path('', views.post_list, name='post_list'),
 ]
-=======
-#check it out joe
-
-urlpatterns = [
-    path('', views.post_list, name='post_list'),
-]
->>>>>>> 07f86da84b28294428f5f8969c322e0c85733a2a
